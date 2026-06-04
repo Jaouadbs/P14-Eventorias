6c8693bf84f20ad8c4ed08f6b2bf723d22f14e57
Jaouadbs <wk.boussalah@gmail.com>
2026-06-04 19:32:31 +0200

    Initial Commit

---
+++ P14-Eventorias/P14_EventoriasApp.swift
@@ -0,0 +1,17 @@
+//
+//  P14_EventoriasApp.swift
+//  P14-Eventorias
+//
+//  Created by Jaouad on 04/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct P14_EventoriasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ P14-EventoriasUITests/P14_EventoriasUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  P14_EventoriasUITestsLaunchTests.swift
+//  P14-EventoriasUITests
+//
+//  Created by Jaouad on 04/06/2026.
+//
+
+import XCTest
+
+final class P14_EventoriasUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
